f25bfeb68e465ceffa13bb0ad0830de23b02aa1e
Seyren <phanuphong.du@gmail.com>
2015-08-24 01:32:41 +0700

    create admin system folder

---
+++ admin/authen.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(0);
+if(!isset($_SESSION)){
+    session_start();
+}
+if ($_SESSION['userLogin'] != 'login') {
+    echo '<script>window.location.href="login.php"</script>';
+    die();
+} else {
+    include './dbconnect.php';
+    include './function.php';
+}
+?>
---
+++ admin/footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <div class="footer-inner">
+        2015 &copy; ClickBank Tools by Seyren
+    </div>
+    <div class="footer-tools">
+        <span class="go-top">
+            <i class="icon-angle-up"></i>
+        </span>
+    </div>
+</div>
