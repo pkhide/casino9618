6b236e84866a8fe69c0ca3d709839f1c99c4f75d
Seyren <phanuphong.du@gmail.com>
2015-08-29 01:28:49 +0700

    fix function send and check credit sms

--- admin/process_ajax.php
+++ admin/process_ajax.php
@@ -10,4 +10,51 @@ if (!empty($_GET['check_admin_username'])) {
     }
 }
 
+if (!empty($_POST['send_sms'])) {
+    $data = array(
+        'username' => $_POST['user_name'],
+        'password' => $_POST['user_password'],
+        'msisdn' => $_POST['user_name'],
+        'message' => $_POST['message'],
+        'sender' => $_POST['sender_name'],
+        'ScheduledDelivery' => '',
+        'force' => 'standard'
+    );
+    $data_string = http_build_query($data);
+    $agent = "ThaiBulkSMS API PHP Client";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://www.thaibulksms.com/sms_api.php');
+    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+    $xml_result = curl_exec($ch);
+    $code = curl_getinfo($ch);
+    curl_close($ch);
+    if ($code['http_code'] == 200) {
+        echo $xml_result;
+    } else {
+        echo $code['http_code'];
+    }
+}
+
+if (!empty($_POST['check_sms_credit'])) {
+    $data_string = "username=$_POST[user_name]&password=$_POST[user_password]&tag=credit_remain";
+    $agent = "ThaiBulkSMS API PHP Client";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://www.thaibulksms.com/sms_api.php');
+    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+    $result = curl_exec($ch);
+    $info = curl_getinfo($ch);
+    curl_close($ch);
+
+    if ($info['http_code'] == 200) {
+        echo $result;
+    } else {
+        echo $info['http_code'];
+    }
+}
 ?>
